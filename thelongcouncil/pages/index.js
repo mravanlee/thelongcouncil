0f7a24c1d445506a57007871e6e345277656417f
mravanlee <77342376+mravanlee@users.noreply.github.com>
2026-04-18 00:36:17 +0200

    Replace 'Raise an issue' link with 'About' link

--- thelongcouncil/pages/index.js
+++ thelongcouncil/pages/index.js
@@ -385,7 +385,8 @@ export default function Home() {
       <nav className="nav">
         <Link href="/council" className="nav-link">The Council</Link>
         <span className="nav-link nav-disabled" title="Coming soon">The Archive</span>
-        <a className="nav-raise nav-active-raise" onClick={reset}>Raise an issue</a>
+        <Link href="/about" className="nav-link">About</Link>
+        <a className="nav-raise" onClick={reset}>Raise an issue</a>
       </nav>
 
       {screen === 'landing' && (
